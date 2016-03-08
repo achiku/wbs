88441fdd5a489f6eaa8a2c4ecdcf64f33ade7091
Akira Chiku <akira.chiku@gmail.com>
2016-03-08 20:51:53 +0900

    Fix toml element names

--- config.go
+++ config.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
-	"os"
 
 	"github.com/BurntSushi/toml"
 )
@@ -17,29 +15,18 @@ type WbsConfig struct {
 	BuildCommand             string   `toml:"build_command"`
 	BuildOptions             []string `toml:"build_options"`
 	StartOptions             []string `toml:"start_options"`
-	WatchTargetDirs          []string `toml:"watch_target_dir"`
-	WatchExcludeDirs         []string `toml:"watch_exclude_dir"`
+	WatchTargetDirs          []string `toml:"watch_target_dirs"`
+	WatchExcludeDirs         []string `toml:"watch_exclude_dirs"`
 	WatchFileExt             []string `toml:"watch_file_ext"`
 	WatchFileExcludePatterns []string `toml:"watch_file_exclude_pattern"`
 }
 
 // NewWbsConfig create wbs config struct
 func NewWbsConfig(configFilePath string) (*WbsConfig, error) {
-	f, err := os.Open(configFilePath)
-	if err != nil {
-		log.Fatalf("failed to open config file: %s: %s", configFilePath, err)
-		return nil, err
-	}
-	defer f.Close()
-	buf, err := ioutil.ReadAll(f)
-	if err != nil {
-		log.Fatalf("failed to read config file: %s", err)
-		return nil, err
-	}
 	var config WbsConfig
 	// set default value
 	config.RestartProcess = true
-	if err := toml.Unmarshal(buf, &config); err != nil {
+	if _, err := toml.DecodeFile(configFilePath, &config); err != nil {
 		log.Fatalf("failed to create Config from file: %s", err)
 		return nil, err
 	}
